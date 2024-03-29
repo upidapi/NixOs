f8a30398f26d24bf4efd4498cf6a8cc3ec13317b
upidapi <73248862+upidapi@users.noreply.github.com>
2024-03-29 22:10:25 +0000

    Update install-script.sh

--- test/install-script.sh
+++ test/install-script.sh
@@ -1,6 +1,6 @@
 sudo mkdir /mnt/persist
-sudo git clone https://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@github.com/upidapi/NixOs.git /mnt/persist/
-sudo cd /mnt/persist/NixOs
+sudo git clone https://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@github.com/upidapi/NixOs.git /mnt/persist/nixos
+sudo cd /mnt/persist/nixos
 
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./hosts/default/disko.nix
 # sudo nixos-generate-config --no-filesystems --root /mnt

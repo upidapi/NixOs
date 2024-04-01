5717c4268fac66b137e59902e110c60a3f2c025f
upidapi <73248862+upidapi@users.noreply.github.com>
2024-04-01 14:38:44 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
 

30d2cebb3fd7b805daf893b2aa93c5b867773be8
upidapi <videw@icloud.com>
2024-04-01 16:17:51 +0200

    save

--- hosts/default/disko.nix
+++ hosts/default/disko.nix
@@ -14,7 +14,7 @@
           esp = {
             name = "ESP";
             size = "500M";
-            type §k= "EF00";
+            type = "EF00";
             content = {
               type = "filesystem";
               format = "vfat";

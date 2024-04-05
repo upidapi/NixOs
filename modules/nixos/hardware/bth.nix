3223ad219f75fb5235f314c6b6647b09570b0310
upidapi <videw@icloud.com>
2024-04-05 22:32:23 +0200

    removed unecesary bth settings

--- modules/nixos/hardware/bth.nix
+++ modules/nixos/hardware/bth.nix
@@ -11,19 +11,23 @@ in {
   options.modules.nixos.hardware.bth =
     mkEnableOpt "enables bluetooth for the system";
 
+  # NOTE: it seems that bth wont work when there's no
+  #  audio sources, i.e nothing playing audia
+
   config = mkIf cfg.enable {
     hardware.bluetooth = {
       enable = true; # enables support for Bluetooth
       powerOnBoot = true; # powers up the default Bluetooth controller on boot
 
-      settings = {
-        General = {
-          Experimental = "true";
-
-          ControllerMode = "bredr";
-          AutoEnable = "true";
-        };
-      };
+      # doesn't fix anything'
+      # settings = {
+      #   General = {
+      #      Experimental = "true";
+      #
+      #     ControllerMode = "bredr";
+      #     AutoEnable = "true";
+      #   };
+      # };
     };
 
     services.blueman.enable = true;

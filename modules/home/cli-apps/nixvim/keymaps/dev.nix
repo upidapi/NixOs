9464079e1b6532c98ecc9c9851f1d9a6b3462e50
upidapi <videw@icloud.com>
2024-04-23 17:35:42 +0200

    TEST neovim binds
    info:
      Profile: default
      Gen: 159 -> 160
      NixOs: 24.05.20240419.5c24cf2
      Kernel: 6.6.28

--- modules/home/cli-apps/nixvim/keymaps/dev.nix
+++ modules/home/cli-apps/nixvim/keymaps/dev.nix
@@ -1,25 +1,65 @@
-{
+let
+  mkMultiCmd = {
+    key,
+    action,
+    override ? {},
+  }: [
+    ({
+        mode = "n";
+        options.silent = true;
+        key = key;
+        action = action;
+      }
+      // override)
+    ({
+        mode = "i";
+        options.silent = true;
+        key = key;
+        action = "<esc><cmd>${action}<CR>i";
+      }
+      // override)
+  ];
+
+  mkMultiCmds = data: (
+    builtins.concatLists
+    (
+      builtins.map
+      (
+        sub_data:
+          mkMultiCmd (
+            if builtins.typeOf sub_data == "set"
+            then sub_data
+            else {
+              key = builtins.elemAt sub_data 0;
+              action = builtins.elemAt sub_data 1;
+            }
+          )
+      )
+      data
+    )
+  );
+in {
   programs.nixvim = {
-    keymaps = [
-      # formatt file
-      {
-        # mode = ["n" "i"];
-        options.silent = true;
-        key = "<F1>";
-        action = ":xa<CR>";
-      }
-      {
-        # mode = ["n" "i"];
-        options.silent = true;
-        key = "<F2>";
-        action = ":wa<CR>";
-      }
-      {
-        # mode = ["n" "i"];
-        options.silent = true;
-        key = "<F3>";
-        action = ":lua require('conform').format()";
-      }
-    ];
+    keymaps =
+      [
+        # formatt file
+
+        {
+          mode = "i";
+          key = "<F2>";
+          action = "<esc><cmd>wa<CR>i";
+        }
+        {
+          mode = "i";
+          options.silent = true;
+          key = "<F2>";
+          action = "<esc><cmd>wa<CR>i";
+        }
+      ]
+      ++ mkMultiCmds [
+        ["<F1>" "xa"]
+        ["<F2>" "wa"]
+        # ["<F3>" ":lua require('conform').format()"]
+      ];
   };
 }

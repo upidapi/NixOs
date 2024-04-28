7e0b979b15beac94bc78934f09e68d2052bcc431
upidapi <videw@icloud.com>
2024-04-28 13:15:08 +0200

    change font size
    info:
      Profile: laptop
      Gen: 6 -> 7
      NixOs: 24.05.20240419.5c24cf2
      Kernel: 6.6.28

--- modules/home/apps/alacritty.nix
+++ modules/home/apps/alacritty.nix
@@ -13,12 +13,13 @@ in {
     "Whether or not to enable alacritty, a terminal emulator";
 
   config = mkIf cfg.enable {
+    # i dont think this is actualy the defautl settings
     # https://github.com/TwiggieSmallz/Default-Alacritty-TOML-Config/blob/main/alacritty.toml
 
     programs.alacritty = {
       enable = true;
       settings = {
-        font.size = 14;
+        font.size = 9;
       };
     };
 

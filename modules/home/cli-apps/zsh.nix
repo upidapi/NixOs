e8fd4d464c9682c4a13e9452b753a8826516ff81
upidapi <videw@icloud.com>
2024-06-03 17:44:03 +0200

    tried fixinf the zsh hist
    info:
      Profile: default
      Gen: 328 -> 329
      NixOs: 24.11.20240529.ad57eef
      Kernel: 6.6.32

--- modules/home/cli-apps/zsh.nix
+++ modules/home/cli-apps/zsh.nix
@@ -38,7 +38,7 @@ in {
       # dotDir = "./config/zsh";
       history = {
         size = 10000;
-        path = "$HOME/.zsh/history";
+        path = "$HOME/.zsh-test/history";
       };
     };
 

4f5f51739cc35e8138b3736516e350a4c6b8f86c
upidapi <videw@icloud.com>
2024-06-28 14:36:36 +0200

    tried only enabeling the key placement
    info:
      Profile: default
      Gen: 370 -> 371
      NixOs: 24.11.20240622.a71e967
      Kernel: 6.6.35

--- modules/nixos/system/misc/sops.nix
+++ modules/nixos/system/misc/sops.nix
@@ -37,14 +37,14 @@ in {
       #  and the .ssh key should probably be user not host specific
 
       secrets = {
-        /*
         "sops-age-key" = {
           path = "/home/upidapi/.config/sops/age/keys.txt";
           owner = "upidapi";
           mode = "0400";
         };
 
-        */
+        /*
+        # this causes (at least) /home/upidapi/.config to not be generated
 
         # the key names equate to the key names for the sops keys
         "github-key" = {
@@ -61,6 +61,7 @@ in {
           # + "${config.modules.nixos.host-name}";
           # format = "binary";
         };
+        */
       };
     };
   };

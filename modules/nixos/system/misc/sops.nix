dfbbceee5512861c981a85b6b2604ff0705cb736
upidapi <videw@icloud.com>
2024-06-28 14:59:30 +0200

    only ssh
    info:
      Profile: default
      Gen: 373 -> 374
      NixOs: 24.11.20240622.a71e967
      Kernel: 6.6.35

--- modules/nixos/system/misc/sops.nix
+++ modules/nixos/system/misc/sops.nix
@@ -37,13 +37,13 @@ in {
       #  and the .ssh key should probably be user not host specific
 
       secrets = {
+        /*
         "sops-age-key" = {
           path = "/home/upidapi/.config/sops/age/keys.txt";
           owner = "upidapi";
           mode = "0400";
         };
 
-        /*
         # this causes (at least) /home/upidapi/.config to not be generated
 
         # the key names equate to the key names for the sops keys
@@ -52,7 +52,7 @@ in {
           owner = "upidapi";
           mode = "0400";
         };
-
+        */
         "hosts/upidapi-nix-pc" = {
           path = "/home/upidapi/.ssh/id_ed25519";
           owner = "upidapi";
@@ -61,7 +61,6 @@ in {
           # + "${config.modules.nixos.host-name}";
           # format = "binary";
         };
-        */
       };
     };
   };

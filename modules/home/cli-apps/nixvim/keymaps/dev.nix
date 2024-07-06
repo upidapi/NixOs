949be49f1001ac84161643b38a2a596a05d6fcf8
upidapi <videw@icloud.com>
2024-07-06 03:43:10 +0200

    fix the leader keybinds
    info:
      Profile: upinix-pc
      Gen: 408 -> 409
      NixOs: 24.11.20240703.9f4128e
      Kernel: 6.6.36

--- modules/home/cli-apps/nixvim/keymaps/dev.nix
+++ modules/home/cli-apps/nixvim/keymaps/dev.nix
@@ -17,20 +17,23 @@ let
     }
     // override);
 
+  strToChars = s: builtins.genList (p: builtins.substring p 1 s) (builtins.stringLength s);
+
   mkMultiCmd = {
-    # modes,
+    modes,
     key,
     action,
     override ? {},
-  }: [
-    (mkCmd
-      {
-        mode = "n";
+  }:
+    builtins.map
+    (mode:
+      mkCmd {
+        inherit mode;
+        inherit key;
         inherit action;
         inherit override;
-        inherit key;
       })
-  ];
+    (strToChars modes);
 
   mkMultiCmds = data: (
     builtins.concatLists
@@ -42,45 +45,52 @@ let
             if builtins.typeOf sub_data == "set"
             then sub_data
             else {
-              key = builtins.elemAt sub_data 0;
-              action = builtins.elemAt sub_data 1;
+              modes = builtins.elemAt sub_data 0;
+              key = builtins.elemAt sub_data 1;
+              action = builtins.elemAt sub_data 2;
             }
           )
       )
       data
     )
   );
+
+  prependList = pre: things:
+    builtins.map
+    (thing: [pre] ++ thing)
+    things;
 in {
   programs.nixvim = {
     keymaps =
       []
-      ++ mkMultiCmds [
-        /*
-        ["<F1>" "xa"]
-        ["<F2>" "wa"]
-        ["<F3>" ":lua require('conform').format()"]
+      ++ mkMultiCmds (
+        (prependList "ni" [
+          ["<F1>" "xa"]
+          ["<F2>" "wa"]
+          ["<F3>" ":lua require('conform').format()"]
+        ])
+        ++ (prependList "n" [
+          # dap
+          ["<leader>db" "DapToggleBreakpoint"]
+          ["<leader>dt" "DapTerminate"]
+          ["<leader>dc" "DapContinue"]
+          ["<leader>di" "DapStepInto"]
+          ["<leader>do" "DapStepOut"]
+          ["<leader>dv" "DapStepOver"]
 
-        # dap
-        ["<leader>db" "DapToggleBreakpoint"]
-        ["<leader>dt" "DapTerminate"]
-        ["<leader>dc" "DapContinue"]
-        ["<leader>di" "DapStepInto"]
-        ["<leader>do" "DapStepOut"]
-        ["<leader>dv" "DapStepOver"]
+          # neotree
+          ["<leader>t" "Neotree toggle"]
+          # ["<leader>fu" "Neotree"]  # neotree update
 
-        # neotree
-        ["<leader>t" "Neotree toggle"]
-        # ["<leader>fu" "Neotree"]  # neotree update
-
-        # telescope
-        ["<leader>ft" "TodoTelescope"]
-        ["<leader>fc" "Telescope git_bcommits"]
-        ["<leader>fC" "Telescope git_commits"]
-        ["<leader>fb" "Telescope buffers"]
-        ["<leader>ff" "Telescope find_files"]
-        ["<leader>fg" "Telescope live_grep"]
-        ["<leader>fz" "Telescope current_buffer_fuzzy_find"]
-        */
-      ];
+          # telescope
+          ["<leader>ft" "TodoTelescope"]
+          ["<leader>fc" "Telescope git_bcommits"]
+          ["<leader>fC" "Telescope git_commits"]
+          ["<leader>fb" "Telescope buffers"]
+          ["<leader>ff" "Telescope find_files"]
+          ["<leader>fg" "Telescope live_grep"]
+          ["<leader>fz" "Telescope current_buffer_fuzzy_find"]
+        ])
+      );
   };
 }

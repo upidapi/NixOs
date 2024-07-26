a29418d84cec6c02613f65f80fc04bd00d8f9576
upidapi <videw@icloud.com>
2024-07-26 23:32:30 +0200

    fix monitor name?
    info:
      Profile: upinix-laptop
      Gen: 4 -> 5
      NixOs: 24.11.20240723.dc14ed9
      Kernel: 6.10.0

--- hosts/upinix-laptop/config.nix
+++ hosts/upinix-laptop/config.nix
@@ -41,12 +41,13 @@ in {
 
       monitors = [
         {
-          name = "eDP-1";
+          name = "eDP-2";
           width = 1920;
           height = 1080;
-          refreshRate = 60;
+          refreshRate = 240;
           x = 0;
           y = 0;
+
           workspace = 1;
           primary = true;
         }

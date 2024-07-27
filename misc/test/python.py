8d7e03a9983594282ee78d82820b2c2d16b0c4c0
upidapi <videw@icloud.com>
2024-07-27 13:58:52 +0200

    better brighness ctl
    info:
      Profile: upinix-laptop
      Gen: 13 -> 14
      NixOs: 24.11.20240723.dc14ed9
      Kernel: 6.10.0

--- misc/test/python.py
+++ misc/test/python.py
@@ -1,6 +1,18 @@
+
+
 s = 20
-m = 100 # $(brightnessctl max); 
+m = 255 # $(brightnessctl max); 
+c = 0
+
+
+def a(d):
+    b = 1 + 1 / s
+    p = (b ** (c / m * s + d) - 1) / (b ** s - 1)
+    return round(p * m)
+
 for i in range(s + 1):
+    # print(c)
+    # c = a(1)
     c = m / s * i # $(brightnessctl get); 
     b = 1 + 1 / s
     p = (b ** (c / m * s) - 1) / (b ** s - 1)

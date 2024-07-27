285378f5266a9e8293356cdb7510453d2b11d5b6
upidapi <videw@icloud.com>
2024-07-27 17:59:03 +0200

    improve locale
    info:
      Profile: upinix-laptop
      Gen: 16 -> 17
      NixOs: 24.11.20240723.dc14ed9
      Kernel: 6.10.0

--- modules/nixos/os/environment/locale.nix
+++ modules/nixos/os/environment/locale.nix
@@ -18,13 +18,13 @@ in {
       extraLocaleSettings = {
         LC_ADDRESS = "sv_SE.UTF-8";
         LC_IDENTIFICATION = "sv_SE.UTF-8";
-        LC_MEASUREMENT = "sv_SE.UTF-8";
+        LC_MEASUREMENT = "en_GB.UTF-8";
         LC_MONETARY = "sv_SE.UTF-8";
         LC_NAME = "sv_SE.UTF-8";
-        LC_NUMERIC = "sv_SE.UTF-8";
-        LC_PAPER = "sv_SE.UTF-8";
+        LC_NUMERIC = "en_GB.UTF-8";
+        LC_PAPER = "en_GB.UTF-8";
         LC_TELEPHONE = "sv_SE.UTF-8";
-        LC_TIME = "sv_SE.UTF-8";
+        LC_TIME = "en_GB.UTF-8";
       };
     };
   };

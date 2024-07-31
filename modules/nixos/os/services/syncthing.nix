59702d076a62de2903c8a6be9faff52da8c3e68e
upidapi <videw@icloud.com>
2024-07-31 22:55:04 +0200

    debug: syncthing
    info:
      Profile: upinix-laptop
      Gen: 43 -> 44
      NixOs: 24.11.20240725.5ad6a14
      Kernel: 6.10.0

--- modules/nixos/os/services/syncthing.nix
+++ modules/nixos/os/services/syncthing.nix
@@ -52,7 +52,8 @@ in {
           home-persist = {
             path = "~/persist";
 
-            devices = devIds;
+            # devices = devIds;
+            devices = ["upinix-pc"];
 
             versioning = {
               type = "staggered";

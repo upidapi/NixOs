81bc83ed238e84b9de4df9c4c22f6bfc97eabeda
upidapi <videw@icloud.com>
2024-08-05 00:52:25 +0200

    restructure nvf try to fix spell
    info:
      Profile: upinix-pc
      Gen: 505 -> 506
      NixOs: 24.11.20240731.9f918d6
      Kernel: 6.6.43

---
+++ modules/home/cli-apps/nvf/config/default.nix
@@ -0,0 +1,293 @@
+{
+  config,
+  inputs,
+  lib,
+  my_lib,
+  ...
+}: let
+  inherit (lib) mkIf mkMerge;
+  inherit (my_lib.opt) enable disable;
+  inherit (inputs.nvf.lib.nvim.binds) mkBinding;
+  cfg = config.modules.home.cli-apps.nvf;
+in {
+  imports = [
+    ./cmp.nix
+    ./dap.nix
+    ./text.nix
+    ./lang.nix
+    ./notes.nix
+  ];
+
+  config = mkIf cfg.enable {
+    programs.nvf = {
+      settings.vim = {
+        # ? syntaxHighlighting = true;
+        hideSearchHighlight = false; # ?
+        searchCase = "sensitive";
+        # ? showSignColumn = false
+
+        splitRight = true;
+        lineNumberMode = "relNumber";
+        wordWrap = true;
+
+        tabWidth = 4;
+
+        # n	Normal mode
+        # v	Visual mode
+        # i	Insert mode
+        # c	Command-line mode
+        # h	all previous modes when editing a help file
+        # a	all previous modes
+        # r	for hit-enter and more-prompt prompt
+        # TODO: submit pr since options only allow for single char
+        # mouseSupport = "nvchr";
+        disableArrows = false;
+
+        leaderKey = " ";
+        mapTimeout = 500;
+
+        binds = {
+          # ? cheatsheet = enable;
+          # ? whichKey = enable;
+        };
+
+        dashboard = {
+          # ? alpha = enable;
+        };
+
+        filetree = {
+          # TODO: neo-tree or nvimTree ?
+          #  nvimTree has a lot better support (1.5k lines of docs lol)
+          neo-tree = {
+            enable = true;
+            setupOpts = {
+              window.width = 30;
+            };
+          };
+        };
+
+        # TODO: neorg
+        # https://github.com/Soliprem/nix-config/blob/7430b9a50de3988d08b4d2b80712c3849b0b81ea/home-manager/nvim.nix#L263
+
+        # git intergration
+        # TODO: binds?
+        git.vim-fugitive = enable;
+
+        lsp = {
+          enable = true;
+          formatOnSave = false;
+
+          # TODO: config this?
+          # lightbulb = enable;
+
+          # TODO: config this?
+          lspSignature = enable;
+
+          # already enabled by lang.nix
+          # lspconfig = enable;
+
+          # pictograms for lsp options
+          lspkind = enable;
+
+          # inline lsp disagnostcs
+          lsplines = enable;
+
+          # on linke 6139
+          # reading about lspsaga
+          # TODO: binds
+          # TODO: remove this?
+          #  its probably unecisary
+          lspsaga = enable;
+
+          # enabled automatically
+          # null-ls = enable;
+
+          mappings = {
+            # TODO:
+          };
+
+          # trouble = enable;
+        };
+
+        notify = {
+          # ? nvim-notify = enable;
+        };
+
+        projects.project-nvim = enable;
+
+        snippets = {
+          # TODO: luasnip
+        };
+
+        spellcheck = {
+          enable = true;
+          languages = ["en" "sv"];
+          # TODO: programmingWordlist = enable;
+          # i.e. vim-dirtytalk = enable;
+        };
+
+        statusline = {
+          lualine = enable;
+          # TODO: config
+        };
+
+        # here
+        tabline = {
+          nvimBufferline = {
+            enable = true;
+            mappings = {
+              # TODO:
+            };
+          };
+        };
+
+        maps.normal = mkMerge [
+          (mkBinding "<leader>fz" "Telescope current_buffer_fuzzy_find" "telescope fuzzy find")
+
+          # neo tre
+          (mkBinding "<leader>tt" "Neotree toggle" "toggle neo-tree")
+          (mkBinding "<leader>tu" "Neotree" "goto neo-tree") # neotree update
+          (mkBinding "<leader>tr" "Neotree reveal" "show self in neo-tree") # neotree update
+        ];
+
+        telescope = {
+          enable = true;
+          mappings = {
+            findProjects = "<leader>fp"; # "<leader>fp";
+            findFiles = "<leader>ff"; # "<leader>ff";
+            buffers = "<leader>fb"; # "<leader>fb";
+
+            liveGrep = "<leader>"; # "<leader>fg";
+            helpTags = null; # "<leader>fh";
+            open = null; # "<leader>ft";
+
+            diagnostics = null; # "<leader>fld";
+            treesitter = null; # "<leader>fs";
+
+            gitCommits = "<leader>fC"; # "<leader>fvcw";
+            gitBufferCommits = "<leader>fc"; # "<leader>fvcb";
+            gitBranches = null; # "<leader>fvb";
+            gitStatus = null; # "<leader>fvs";
+            gitStash = null; # "<leader>fvx";
+
+            lspDocumentSymbols = null; # "<leader>flsb";
+            lspWorkspaceSymbols = null; # "<leader>flsw";
+            lspReferences = null; # "<leader>flr";
+            lspImplementations = null; # "<leader>fli";
+            lspDefinitions = null; # "<leader>flD";
+            lspTypeDefinitions = null; # "<leader>flt";
+          };
+        };
+
+        terminal = {
+          toggleterm = {
+            enable = true;
+            mappings = {
+              open = "<c-t>";
+            };
+          };
+        };
+
+        theme = {
+          enable = true;
+          name = "tokyonight";
+          transparent = false;
+          style = "moon"; # maybe storm or night
+        };
+
+        treesitter = {
+          enable = true;
+          autotagHtml = true;
+
+          # show shat scopes you're in as horizontal lines
+          # context = enable;
+
+          # ? fold = true; (prob use nvim-ufo insted)
+
+          # aleady set by lang.nix
+          # grammars = []
+
+          incrementalSelection = enable;
+          indent = enable;
+
+          mappings = {
+            # TODO:
+          };
+        };
+
+        ui = {
+          borders = disable;
+          breadcrumbs = disable;
+
+          colorizer = enable;
+          illuminate = disable;
+
+          # alternative ui
+          # noice = enable;
+
+          smartcolumn = {
+            enable = true;
+            setupOpts.colorcolumn = ["80" "100"];
+          };
+        };
+
+        utility = {
+          ccc = {
+            enable = true;
+            mappings = {
+              decrease10 = "<H>";
+              increase10 = "<L>";
+              quit = "<Esc>";
+            };
+          };
+
+          diffview-nvim = enable;
+          # icon-picker = enable;
+
+          images.image-nvim = {
+            # doesn't work
+            # enable = true;
+            # setupOpts.backend = "ueberzug"; # TODO: "kitty"; ?
+          };
+
+          # prob not
+          # motion.leap = enable;
+
+          preview.markdownPreview = enable;
+
+          # surround = enable;
+        };
+
+        visuals = {
+          # TODO: nvim ufo
+
+          enable = true;
+
+          # give characters gravity
+          cellularAutomaton = {
+            enable = true;
+
+            # add game_of_life?
+            mappings.makeItRain = "<leader>fml";
+          };
+
+          cursorline = enable;
+
+          # for notifications
+          # fidget-nvim = enable; ?
+
+          highlight-undo = enable;
+
+          indentBlankline = {
+            enable = true;
+            # debounce = 0; ?
+          };
+
+          nvimWebDevicons = enable;
+
+          # scrollBar = enable; ?
+        };
+      };
+    };
+  };
+}
--- modules/home/cli-apps/nvf/default.nix
+++ modules/home/cli-apps/nvf/default.nix
@@ -1,3 +1,4 @@
+# 95% of this file is directly copied from raf / NotAShelf
 {
   config,
   inputs,
@@ -6,83 +7,58 @@
   pkgs,
   ...
 }: let
-  inherit (lib) mkIf mkMerge;
-  inherit (my_lib.opt) mkEnableOpt enable disable;
-  inherit (inputs.nvf.lib.nvim.binds) mkBinding;
+  inherit (lib) mkIf;
+  inherit (my_lib.opt) mkEnableOpt;
+
+  inherit (builtins) filter map toString path;
+  inherit (lib.filesystem) listFilesRecursive;
+  inherit (lib.strings) hasSuffix fileContents;
+  inherit (lib.attrsets) genAttrs;
+
+  inherit (inputs.nvf.lib.nvim.dag) entryBefore;
+
+  mkRuntimeDir = name: let
+    finalPath = ./runtime + /${name};
+  in
+    path {
+      name = "nvim-runtime-${name}";
+      path = toString finalPath;
+    };
+
   cfg = config.modules.home.cli-apps.nvf;
 in {
   imports = [
     inputs.nvf.homeManagerModules.default
-    ./cmp.nix
-    ./dap.nix
-    ./text.nix
-    ./lang.nix
-    ./notes.nix
+    ./config
   ];
 
   options.modules.home.cli-apps.nvf =
     mkEnableOpt "enables nvf a neovim distro powerd by nix";
 
   config = mkIf cfg.enable {
-    /*
-    home.sessionVariables = {
-      EDITOR = "nvim";
-    };
-    */
-
-    # home.packages = [inputs.nvf.packages.${pkgs.stdenv.system}.docs-manpages];
-
     programs.nvf = {
       enable = true;
       enableManpages = true;
       defaultEditor = true;
 
       settings.vim = {
+        package = pkgs.neovim-unwrapped;
+
         viAlias = false;
         vimAlias = false;
 
         # vim.globals can be used to set vim.g.<name>
 
         # defaults
-        # withNodeJs = false;
-        # withPython3 = false;
-        # withRuby = true;
+        withNodeJs = false;
+        withPython3 = false;
+        withRuby = false;
 
-        # ? syntaxHighlighting = true;
-        hideSearchHighlight = false; # ?
-        searchCase = "sensitive";
-        # ? showSignColumn = false
+        preventJunkFiles = true;
+        useSystemClipboard = true;
 
-        splitRight = true;
-        lineNumberMode = "relNumber";
-        wordWrap = true;
-
-        useSystemClipboard = false; # ?
-
-        tabWidth = 4;
-
-        # n	Normal mode
-        # v	Visual mode
-        # i	Insert mode
-        # c	Command-line mode
-        # h	all previous modes when editing a help file
-        # a	all previous modes
-        # r	for hit-enter and more-prompt prompt
-        # TODO: submit pr since options only allow for single char
-        # mouseSupport = "nvchr";
-        disableArrows = false;
-
-        leaderKey = " ";
-        mapTimeout = 500;
-
-        binds = {
-          # ? cheatsheet = enable;
-          # ? whichKey = enable;
-        };
-
-        dashboard = {
-          # ? alpha = enable;
-        };
+        enableLuaLoader = true;
+        enableEditorconfig = true;
 
         debugMode = {
           enable = false;
@@ -90,240 +66,39 @@ in {
           logFile = "/tmp/nvim.log";
         };
 
-        filetree = {
-          # TODO: neo-tree or nvimTree ?
-          #  nvimTree has a lot better support (1.5k lines of docs lol)
-          neo-tree = {
-            enable = true;
-            setupOpts = {
-              window.width = 30;
-            };
-          };
-        };
+        # sv.utf-8.spl
 
-        # TODO: neorg
-        # https://github.com/Soliprem/nix-config/blob/7430b9a50de3988d08b4d2b80712c3849b0b81ea/home-manager/nvim.nix#L263
-
-        # git intergration
-        # TODO: binds?
-        git.vim-fugitive = enable;
-
-        lsp = {
-          enable = true;
-          formatOnSave = false;
-
-          # TODO: config this?
-          # lightbulb = enable;
-
-          # TODO: config this?
-          lspSignature = enable;
-
-          # already enabled by lang.nix
-          # lspconfig = enable;
-
-          # pictograms for lsp options
-          lspkind = enable;
-
-          # inline lsp disagnostcs
-          lsplines = enable;
-
-          # on linke 6139
-          # reading about lspsaga
-          # TODO: binds
-          # TODO: remove this?
-          #  its probably unecisary
-          lspsaga = enable;
-
-          # enmabled automatically
-          # null-ls = enable;
-
-          mappings = {
-            # TODO:
-          };
-
-          # trouble = enable;
-        };
-
-        notify = {
-          # ? nvim-notify = enable;
-        };
-
-        projects.project-nvim = enable;
-
-        snippets = {
-          # TODO: luasnip
-        };
-
-        spellcheck = {
-          enable = true;
-          languages = ["en" "sv"];
-          # TODO: programmingWordlist = enable;
-          # i.e. vim-dirtytalk = enable;
-        };
-
-        statusline = {
-          lualine = enable;
-          # TODO: config
-        };
-
-        # here
-        tabline = {
-          nvimBufferline = {
-            enable = true;
-            mappings = {
-              # TODO:
-            };
-          };
-        };
-
-        maps.normal = mkMerge [
-          (mkBinding "<leader>fz" "Telescope current_buffer_fuzzy_find" "telescope fuzzy find")
-
-          # neo tre
-          (mkBinding "<leader>tt" "Neotree toggle" "toggle neo-tree")
-          (mkBinding "<leader>tu" "Neotree" "goto neo-tree") # neotree update
-          (mkBinding "<leader>tr" "Neotree reveal" "show self in neo-tree") # neotree update
+        additionalRuntimePaths = [
+          # (mkRuntimeDir "after")
+          (mkRuntimeDir "spell")
         ];
 
-        telescope = {
-          enable = true;
-          mappings = {
-            findProjects = "<leader>fp"; # "<leader>fp";
-            findFiles = "<leader>ff"; # "<leader>ff";
-            buffers = "<leader>fb"; # "<leader>fb";
-
-            liveGrep = "<leader>"; # "<leader>fg";
-            helpTags = null; # "<leader>fh";
-            open = null; # "<leader>ft";
-
-            diagnostics = null; # "<leader>fld";
-            treesitter = null; # "<leader>fs";
-
-            gitCommits = "<leader>fC"; # "<leader>fvcw";
-            gitBufferCommits = "<leader>fc"; # "<leader>fvcb";
-            gitBranches = null; # "<leader>fvb";
-            gitStatus = null; # "<leader>fvs";
-            gitStash = null; # "<leader>fvx";
-
-            lspDocumentSymbols = null; # "<leader>flsb";
-            lspWorkspaceSymbols = null; # "<leader>flsw";
-            lspReferences = null; # "<leader>flr";
-            lspImplementations = null; # "<leader>fli";
-            lspDefinitions = null; # "<leader>flD";
-            lspTypeDefinitions = null; # "<leader>flt";
-          };
-        };
-
-        terminal = {
-          toggleterm = {
-            enable = true;
-            mappings = {
-              open = "<c-t>";
-            };
-          };
-        };
-
-        theme = {
-          enable = true;
-          name = "tokyonight";
-          transparent = false;
-          style = "moon"; # maybe storm or night
-        };
-
-        treesitter = {
-          enable = true;
-          autotagHtml = true;
-
-          # show shat scopes you're in as horizontal lines
-          # context = enable;
-
-          # ? fold = true; (prob use nvim-ufo insted)
-
-          # aleady set by lang.nix
-          # grammars = []
-
-          incrementalSelection = enable;
-          indent = enable;
-
-          mappings = {
-            # TODO:
-          };
-        };
-
-        ui = {
-          borders = disable;
-          breadcrumbs = disable;
-
-          colorizer = enable;
-          illuminate = disable;
-
-          # alternative ui
-          # noice = enable;
-
-          smartcolumn = {
-            enable = true;
-            setupOpts.colorcolumn = ["80" "100"];
-          };
-        };
-
-        utility = {
-          ccc = {
-            enable = true;
-            mappings = {
-              decrease10 = "<H>";
-              increase10 = "<L>";
-              quit = "<Esc>";
-            };
+        # additional lua configuration that I can append or, to be more
+        # precise, randomly inject into the lua configuration of my Neovim
+        # configuration wrapper. This is recursively read from the lua
+        # directory, so we do not need to use require
+        luaConfigRC = let
+          spellFile = path {
+            name = "nvf-en.utf-8.add";
+            path = ./runtime/spell/en.utf-8.add;
           };
 
-          diffview-nvim = enable;
-          # icon-picker = enable;
+          # get the name of each lua file in the lua directory, where setting
+          # files reside and import them recursively
+          configPaths =
+            filter
+            (hasSuffix ".lua")
+            (map toString (listFilesRecursive ./lua));
 
-          images.image-nvim = {
-            # doesnt work
-            # enable = true;
-            # setupOpts.backend = "ueberzug"; # TODO: "kitty"; ?
-          };
-
-          # prob not
-          # motion.leap = enable;
-
-          preview.markdownPreview = enable;
-
-          # surround = enable;
-        };
-
-        visuals = {
-          # TODO: nvim ufo
-
-          enable = true;
-
-          # give characters gravity
-          cellularAutomaton = {
-            enable = true;
-
-            # add game_of_life?
-            mappings.makeItRain = "<leader>fml";
-          };
-
-          cursorline = enable;
-
-          # for notifications
-          # fidget-nvim = enable; ?
-
-          highlight-undo = enable;
-
-          indentBlankline = {
-            enable = true;
-            # debounce = 0; ?
-          };
-
-          nvimWebDevicons = enable;
-
-          # scrollBar = enable; ?
-        };
-
-        # TODO: indentBlankline
+          # generates a key-value pair that looks roughly as follows:
+          #  `<filePath> = entryAnywhere ''<contents of filePath>''`
+          # which is expected by nvf's modified DAG library
+          luaConfig = genAttrs configPaths (file:
+            entryBefore ["luaScript"] ''
+              ${fileContents file}
+            '');
+        in
+          luaConfig // {spell = "vim.o.spellfile = \"${spellFile}\"";};
       };
     };
   };

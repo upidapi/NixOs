9802542cd3be76a2b1e7d95fd7f4630cca551416
upidapi <videw@icloud.com>
2024-08-07 20:54:27 +0200

    this time actually fix the argpassing to the installer
    info:
      Profile: upinix-pc
      Gen: 548 -> 549
      NixOs: 24.11.20240731.9f918d6
      Kernel: 6.6.43

--- parts/install/default.nix
+++ parts/install/default.nix
@@ -3,13 +3,18 @@
     pkgs,
     # self,
     ...
-  }: {
-    # you can run the installer (run install.sh) with the following command
-    # sudo nix --experimental-features "nix-command flakes" run github:upidapi/nixos
-    packages.install =
-      pkgs.writers.writePython3
+  }: let
+    installerPkg =
+      pkgs.writers.writePython3Bin
       "nixos-installer"
       {flakeIgnore = ["E302" "E303" "E501" "W391" "W293" "W291" "E128" "E124"];}
       (builtins.readFile ./install.py);
+  in {
+    # you can run the installer (run install.sh) with the following command
+    # sudo nix --experimental-features "nix-command flakes" run github:upidapi/nixos
+    apps.install = {
+      type = "app";
+      program = "${installerPkg}/bin/nixos-installer";
+    };
   };
 }

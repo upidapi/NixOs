d4a855ab6f71a78c09605125c3858183786a9616
upidapi <videw@icloud.com>
2024-08-16 23:50:11 +0200

    more hyrland settings, pimarily unmaximise window when a new one is created
    info:
      Profile: upinix-laptop
      Gen: 25 -> 26
      NixOs: 24.11.20240809.5e0ca22
      Kernel: 6.10.3

--- modules/home/desktop/hyprland/config.nix
+++ modules/home/desktop/hyprland/config.nix
@@ -1,6 +1,20 @@
 {...}: {
   wayland.windowManager.hyprland.settings = {
-    "debug:disable_logs" = false;
+    debug.disable_logs = false;
+
+    misc = {
+      # when you create a new windows while you have a maximised window
+      # then its unmaximised
+      new_window_takes_over_fullscreen = 2;
+
+      middle_click_paste = false;
+
+      # idk
+      # mouse_move_enables_dpms = true;
+      # key_press_enables_dpms = true;
+      allow_session_lock_restore = true;
+    };
+
     general = {
       gaps_in = 0;
       gaps_out = 0;

ede591ec5a642582ce0d5733bde4bc1cc0b5586c
upidapi <videw@icloud.com>
2024-09-01 13:38:27 +0200

    add fwupd for bios upgrades
    info:
      Profile: upinix-laptop
      Gen: 54 -> 55
      NixOs: 24.11.20240824.d0e1602
      Kernel: 6.10.6

--- modules/nixos/other.nix
+++ modules/nixos/other.nix
@@ -4,12 +4,14 @@
   lib,
   ...
 }: let
-  inherit (my_lib.opt) mkEnableOpt;
+  inherit (my_lib.opt) mkEnableOpt enable;
   inherit (lib) mkIf;
   cfg = config.modules.nixos.other;
 in {
   options.modules.nixos.other =
     mkEnableOpt "enables config that i've not found a place for";
 
-  config = mkIf cfg.enable {};
+  config = mkIf cfg.enable {
+    services.fwupd = enable;
+  };
 }

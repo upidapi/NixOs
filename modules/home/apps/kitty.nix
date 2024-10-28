ac9ab46230cf28c4a7b45503bc6d82883a0680d2
upidapi <videw@icloud.com>
2024-10-28 02:11:03 +0100

    kitty: remove sound effects and close promt
    info:
      Gen: 833 -> 834
      Kernel: 6.6.57
      NixOs: 24.11.20241018.4c2fcb0
      Profile: upinix-pc
      Type: manuall

--- modules/home/apps/kitty.nix
+++ modules/home/apps/kitty.nix
@@ -5,13 +5,22 @@
   ...
 }: let
   inherit (lib) mkIf;
-  inherit (my_lib.opt) mkEnableOpt enable;
+  inherit (my_lib.opt) mkEnableOpt;
   cfg = config.modules.home.apps.kitty;
 in {
   options.modules.home.apps.kitty = mkEnableOpt "enable kitty, the terminal";
 
   config = mkIf cfg.enable {
-    programs.kitty = enable;
+    programs.kitty = {
+      enable = true;
+      settings = {
+        confirm_os_window_close = 0;
+
+        visual_bell_duration = 0;
+        enable_audio_bell = "no";
+        bell_on_tab = "no";
+      };
+    };
 
     home.sessionVariables = {
       TERMINAL = "kitty";

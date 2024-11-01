be67fccfcd43c7b1e051e51cb5b1fefe6b0a91bd
upidapi <videw@icloud.com>
2024-11-01 16:02:08 +0100

    use custom theme for kitty
    info:
      Gen: 854 -> 855
      Kernel: 6.6.58
      NixOs: 24.11.20241025.18536bf
      Profile: upinix-pc
      Type: manuall

--- modules/home/apps/kitty.nix
+++ modules/home/apps/kitty.nix
@@ -11,6 +11,8 @@ in {
   options.modules.home.apps.kitty = mkEnableOpt "enable kitty, the terminal";
 
   config = mkIf cfg.enable {
+    stylix.targets.kitty.enable = false;
+
     # TODO: remove the stylix theming for the everything but the actual
     #  background
     programs.kitty = {
@@ -23,8 +25,46 @@ in {
         bell_on_tab = "no";
       };
 
-      extraConfig = ''
+      extraConfig = with config.lib.stylix.colors.withHashtag; ''
         map ctrl+shift+r no_op
+
+        # window colors
+        background ${base00}
+        foreground ${base05}
+        selection_background ${base03}
+        selection_foreground ${base05}
+        url_color ${base04}
+        cursor ${base05}
+        cursor_text_color ${base00}
+        active_border_color ${base03}
+        inactive_border_color ${base01}
+        active_tab_background ${base00}
+        active_tab_foreground ${base05}
+        inactive_tab_background ${base01}
+        inactive_tab_foreground ${base04}
+        tab_bar_background ${base01}
+        wayland_titlebar_color ${base00}
+        macos_titlebar_color ${base00}
+
+        # text colors
+        color0 ${base00}
+        color1 ${base08}
+        color2 ${base0B}
+        color3 ${base0A}
+        color4 ${base0D}
+        color5 ${base0E}
+        color6 ${base0C}
+        color7 ${base05}
+
+        # bright
+        # color8 #{{base02-hex}}
+        # color9 #{{base08-hex}}
+        # color10 #{{base0B-hex}}
+        # color11 #{{base0A-hex}}
+        # color12 #{{base0D-hex}}
+        # color13 #{{base0E-hex}}
+        # color14 #{{base0C-hex}}
+        # color15 #{{base07-hex}}
       '';
     };
 

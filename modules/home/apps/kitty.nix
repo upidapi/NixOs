d6b832b9088424581031bb9eb51406368f72f7fd
upidapi <videw@icloud.com>
2024-11-11 20:06:50 +0100

    test
    info:
      Gen: 869 -> 870
      Kernel: 6.6.58
      NixOs: 24.11.20241025.18536bf
      Profile: upinix-pc
      Type: manuall

--- modules/home/apps/kitty.nix
+++ modules/home/apps/kitty.nix
@@ -36,26 +36,55 @@ in {
       };
 
       extraConfig = let
-        config = ''
-          map ctrl+shift+r no_op
-
-          # places the "characters" of the window to the top left
-          # so it padds to the bottom-right
-          placement_strategy top-left  # default center
-        '';
         colorConfig = with config.lib.stylix.colors.withHashtag; ''
-          #   # window colors
-          #
-          #   # bright (real)
-          #   # color8 #{{base02-hex}}
-          #   # color9 #{{base08-hex}}
-          #   # color10 #{{base0B-hex}}
-          #   # color11 #{{base0A-hex}}
-          #   # color12 #{{base0D-hex}}
-          #   # color13 #{{base0E-hex}}
-          #   # color14 #{{base0C-hex}}
-          #   # color15 #{{base07-hex}}
-          # '';
+          # window colors
+          background ${base00}
+          foreground ${base05}
+          selection_background ${base03}
+          selection_foreground ${base05}
+          url_color ${base04}
+          cursor ${base05}
+          cursor_text_color ${base00}
+          active_border_color ${base03}
+          inactive_border_color ${base01}
+          active_tab_background ${base00}
+          active_tab_foreground ${base05}
+          inactive_tab_background ${base01}
+          inactive_tab_foreground ${base04}
+          tab_bar_background ${base01}
+          wayland_titlebar_color ${base00}
+          macos_titlebar_color ${base00}
+
+          # text colors
+          color0 ${base00}
+          color1 ${base08}
+          color2 ${base0B}
+          color3 ${base0A}
+          color4 ${base0D}
+          color5 ${base0E}
+          color6 ${base0C}
+          color7 ${base05}
+
+          # bright text colors
+          color8 ${base00}
+          color9 ${base08}
+          color10 ${base0B}
+          color11 ${base0A}
+          color12 ${base0D}
+          color13 ${base0E}
+          color14 ${base0C}
+          color15 ${base05}
+
+          # bright (real)
+          # color8 #{{base02-hex}}
+          # color9 #{{base08-hex}}
+          # color10 #{{base0B-hex}}
+          # color11 #{{base0A-hex}}
+          # color12 #{{base0D-hex}}
+          # color13 #{{base0E-hex}}
+          # color14 #{{base0C-hex}}
+          # color15 #{{base07-hex}}
+        '';
       in
         colorConfig;
     };

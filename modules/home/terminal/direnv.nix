0d41a74b290904f5c007f0c9f177f17adad2c301
upidapi <videw@icloud.com>
2024-11-11 19:47:22 +0100

    test - uncenter the text in kitty, fix the direnv for persist/sec
    info:
      Gen: 866 -> 867
      Kernel: 6.6.58
      NixOs: 24.11.20241025.18536bf
      Profile: upinix-pc
      Type: manuall

--- modules/home/terminal/direnv.nix
+++ modules/home/terminal/direnv.nix
@@ -20,7 +20,12 @@ in {
 
     # auto enter sec dev shell when going into the sec folder
     home.file."persist/prog/sec/.envrc".text = ''
-      use flake $NIXOS_CONFIG_PATH#sec --builders ""
+      export NIXPKGS_ALLOW_UNFREE=1
+
+      # avoid recursive calls in fhs env
+      if [ -z "$IN_NIX_SHELL" ]; then
+          use flake $NIXOS_CONFIG_PATH#sec --builders "" --impure
+      fi
     '';
   };
 }

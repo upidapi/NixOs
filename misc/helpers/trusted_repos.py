420a5289405752575d9b2b086f4021c0027ab9d1
upidapi <videw@icloud.com>
2025-01-02 14:25:21 +0100

    pull changes
    info:
      Gen: 920 -> 921
      Kernel: 6.6.68
      NixOs: 25.05.20241229.88195a9
      Profile: upinix-pc
      Type: manuall

---
+++ misc/helpers/trusted_repos.py
@@ -0,0 +1,44 @@
+repos = """\
+https://github.com/notashelf/nyx
+https://github.com/nobbz/nixos-config
+https://github.com/fufexan/dotfiles
+https://github.com/mitchellh/nixos-config
+https://github.com/mic92/dotfiles
+https://github.com/workflow/dotfiles
+https://github.com/notohh/snowflake
+https://github.com/misterio77/nix-config
+
+https://github.com/hlissner/dotfiles
+https://github.com/gvolpe/nix-config
+"""
+
+parsed = [
+    x.strip()
+    for x in repos.split("\n")
+    if x.strip() != ""
+]
+
+data = [{
+        "url": data,
+        "host": data.split("/")[2],
+        "user": data.split("/")[3],
+        "name": data.split("/")[4],
+    } for data in parsed
+]
+
+def get_github_search():
+    repos = " OR ".join([f"repo:{d['user']}/{d['name']}" for d in data])
+
+    print(f"lang:nix ({repos})")
+
+# def gen_md_credits():
+#     print(
+#         " - ".join([f"[{d['user']}]({d['url']})" for d in data])
+#     )
+#
+# gen_md_credits()
+
+get_github_search()
+
+
+# lang:nix (repo:notashelf/nyx OR repo:nobbz/nixos-config OR repo:fufexan/dotfiles OR repo:mitchellh/nixos-config OR repo:mic92/dotfiles OR repo:workflow/dotfiles OR repo:notohh/snowflake OR repo:misterio77/nix-config OR repo:hlissner/dotfiles OR repo:gvolpe/nix-config)

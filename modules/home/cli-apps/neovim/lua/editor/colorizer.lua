79c18087d55338ab1648242ad0eefc7947d6ffb6
upidapi <videw@icloud.com>
2025-01-23 11:00:47 +0100

    disable the 0x format for color higliting since meory dumps is in that format
    info:
      Gen: 221 -> 222
      Kernel: 6.12.9
      NixOs: 25.05.20250110.130595e
      Profile: upinix-laptop
      Type: manuall

--- modules/home/cli-apps/neovim/lua/editor/colorizer.lua
+++ modules/home/cli-apps/neovim/lua/editor/colorizer.lua
@@ -1,18 +1,18 @@
 ---- colorizer ----
 -- https://github.com/norcalli/nvim-colorizer.lua
-require("colorizer").setup {
+require("colorizer").setup({
     filetypes = { "*" },
     user_default_options = {
-        RGB = true,      -- #faa hex codes
-        RRGGBB = true,   -- #ffaaaa hex codes
-        RRGGBBAA = true, -- #RRGGBBAA hex codes
-        AARRGGBB = true, -- 0xAARRGGBB hex codes
-        rgb_fn = true,   -- CSS rgb(255, 170, 170) and rgba() functions
-        hsl_fn = true,   -- CSS hsl(0, 100%, 85%) and hsla() functions
+        RGB = true, -- #RGB hex codes
+        RRGGBB = true, -- #RRGGBB hex codes
+        RRGGBBAA = true, -- #RRGGBBAA hex codes #FFFFFF
+        AARRGGBB = false, -- 0xAARRGGBB hex codes0x0011
+        rgb_fn = true, -- CSS rgb(255, 170, 170) and rgba() functions
+        hsl_fn = true, -- CSS hsl(0, 100%, 83%) and hsla() functions
 
-        names = false,   -- "Name" codes like Blue or blue
-        css = false,     -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-        css_fn = false,  -- Enable all CSS *functions*: rgb_fn, hsl_fn
+        names = false, -- "Name" codes like Blue or blue
+        css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
 
         -- Available modes for `mode`: foreground, background,  virtualtext
         mode = "background", -- Set the display mode.
@@ -29,11 +29,10 @@ require("colorizer").setup {
 
         -- update color values even if buffer is not focused
         -- example use: cmp_menu, cmp_docs
-        always_update = false
+        always_update = false,
     },
     -- all the sub-options of filetypes apply to buftypes
     buftypes = {},
-}
-
+})
 
 -- NOTE: ccc breaks nixd :)

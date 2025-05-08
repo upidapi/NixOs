ce9682ff55d77fb7c22cf79642f38a747e6e2027
upidapi <videw@icloud.com>
2025-05-08 18:55:51 +0200

    debu
    info:
      Gen: 282 -> 283
      Kernel: 6.14.3
      NixOs: 25.05.20250424.f771eb4
      Profile: upinix-laptop
      Type: manuall

--- modules/home/misc/vms.nix
+++ modules/home/misc/vms.nix
@@ -128,36 +128,36 @@ in {
           }
         ];
 
-        networks = [
-          {
-            active = true;
-            definition = nlib.network.writeXML {
-              name = "default";
-              uuid = "c4acfd00-4597-41c7-a48e-e2302234fa89";
-              forward = {
-                mode = "nat";
-                nat = {
-                  port = {
-                    start = 1024;
-                    end = 65535;
-                  };
-                };
-              };
-              bridge = {name = "virbr0";};
-              mac = {address = "52:54:00:02:77:4b";};
-              ip = {
-                address = "192.168.74.1";
-                netmask = "255.255.255.0";
-                dhcp = {
-                  range = {
-                    start = "192.168.74.2";
-                    end = "192.168.74.254";
-                  };
-                };
-              };
-            };
-          }
-        ];
+        # networks = [
+        #   {
+        #     active = true;
+        #     definition = nlib.network.writeXML {
+        #       name = "default";
+        #       uuid = "c4acfd00-4597-41c7-a48e-e2302234fa89";
+        #       forward = {
+        #         mode = "nat";
+        #         nat = {
+        #           port = {
+        #             start = 1024;
+        #             end = 65535;
+        #           };
+        #         };
+        #       };
+        #       bridge = {name = "virbr0";};
+        #       mac = {address = "52:54:00:02:77:4b";};
+        #       ip = {
+        #         address = "192.168.74.1";
+        #         netmask = "255.255.255.0";
+        #         dhcp = {
+        #           range = {
+        #             start = "192.168.74.2";
+        #             end = "192.168.74.254";
+        #           };
+        #         };
+        #       };
+        #     };
+        #   }
+        # ];
       };
     };
   };

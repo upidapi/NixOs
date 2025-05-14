1e584a56e845b1af0fdd047639575c711c57b7e4
upidapi <videw@icloud.com>
2025-05-14 22:32:56 +0200

    maybe make it work
    info:
      Gen: 997 -> 998
      Kernel: 6.12.26
      NixOs: 25.05.20250508.dda3dcd
      Profile: upinix-pc
      Type: manuall

--- modules/nixos/homelab/ddclient.nix
+++ modules/nixos/homelab/ddclient.nix
@@ -10,23 +10,35 @@
 in {
   options.modules.nixos.homelab.ddclient = mkEnableOpt "ddclient";
 
+  # REF: https://github.com/hugo-berendi/yomi/blob/29a848529e6b2dcb81f6830a1161cc29c38e3c6d/hosts/nixos/inari/services/ddclient.nix
   config = mkIf cfg.enable {
-    sops.secrets."ddclient-cf-token" = {};
+    sops.secrets."ddclient-cf-token" = {
+      # owner = "ddclient";
+    };
+
+    # it seams like it doesn't work if you don't explicitly provide the zone
+    sops.templates."ddclient.conf".content = ''
+      # General settings
+      cache=/var/lib/ddclient/ddclient.cache
+      foreground=YES
+      use=web, web=https://api.ipify.org/
+      ssl=yes
+      verbose=yes
+      quiet=no
+
+      protocol=cloudflare
+      password=${config.sops.placeholder.ddclient-cf-token}
+      zone=upidapi.com
+      upidapi.com
+
+      protocol=cloudflare
+      password=${config.sops.placeholder.ddclient-cf-token}
+      zone=upidapi.dev
+      upidapi.dev, ssh.upidapi.dev
+    '';
 
     services.ddclient = {
-      enable = true;
-      use = "web, web=https://api.ipify.org/";
-      verbose = true;
-      ssl = true;
-      username = "token";
-      protocol = "cloudflare";
-      zone = "dns.zone";
-      domains = [
-        "upidapi.com"
-        "upidapi.dev"
-        "ssh.upidapi.dev"
-      ];
-      passwordFile = "${config.sops.secrets."ddclient-cf-token".path}";
+      configFile = config.sops.templates."ddclient.conf".path;
     };
   };
 }

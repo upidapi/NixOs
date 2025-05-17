1eb03acbe6463de41e56b2fd4c62187fb3cbaa93
upidapi <videw@icloud.com>
2025-05-17 19:44:12 +0200

    debug
    info:
      Gen: 295 -> 296
      Kernel: 6.14.5
      NixOs: 25.05.20250508.dda3dcd
      Profile: upinix-laptop
      Type: manuall

--- modules/nixos/hardware/keyboard.nix
+++ modules/nixos/hardware/keyboard.nix
@@ -18,8 +18,8 @@ in {
     # "xserver" is actually just the general display server
     # so this actually configs wayland too. :)
     services.xserver.xkb = {
-      layout = "gb,se"; # TODO: maybe remove
-      variant = "";
+      layout = "gb,gb"; # TODO: maybe remove
+      variant = "se";
       options = "compose:menu, grp:alt_space_toggle";
     };
 

eeab5fbcbf4ebbf70239e46bf04d3e74d992e0d7
upidapi <videw@icloud.com>
2025-06-12 15:59:47 +0200

    debug
    info:
      Gen: 379 -> 380
      Kernel: 6.15.1
      NixOs: 25.11.20250607.3e3afe5
      Profile: upinix-laptop
      Type: manuall

--- modules/nixos/homelab/media/jellyseerr/base.nix
+++ modules/nixos/homelab/media/jellyseerr/base.nix
@@ -416,7 +416,7 @@ in {
       systemd.services.jellyseerr.serviceConfig = {
         WorkingDirectory = cfg.dataDir;
         ExecStartPre = "${jellyseerr-init}";
-        ExecStartPost = "bash /home/upidapi/test.sh";
+        ExecStartPost = "${pkgs.bash}/bin/bash /home/upidapi/test.sh";
         LoadCredential = [
           "config:${config.sops.templates."jellyseerr-config.json".path}"
 

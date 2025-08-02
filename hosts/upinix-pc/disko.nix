d8303c1936a0f1c99be053530b041ec382350735
upidapi <videw@icloud.com>
2025-08-02 21:30:35 +0200

    setup raidz on upinix-pc

--- hosts/upinix-pc/disko.nix
+++ hosts/upinix-pc/disko.nix
@@ -1,41 +1,77 @@
+# https://github.com/sodiboo/system/blob/3d970bb172a908e95c46ca097337f858a5f5ad66/core/disko.mod.nix#L31
+# https://github.com/DavHau/hyperconfig/blob/cf7943b64d5011e03f10570e46ea815e114a92f9/machines/manu-nas/disko-config.nix#L97
 {
   disko.devices = {
-    disk.main = {
-      device = "/dev/sda";
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          boot = {
-            name = "boot";
-            size = "1M";
-            type = "EF02";
-          };
-          esp = {
-            name = "ESP";
-            size = "1G";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
+    disk = {
+      main = {
+        device = "/dev/disk/by-id/ata-Samsung_SSD_870_QVO_1TB_S5SVNG0N990909T";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              name = "boot";
+              size = "1M";
+              type = "EF02";
+            };
+            esp = {
+              name = "ESP";
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            /*
+            swap = {
+              size = "4G";
+              content = {
+                type = "swap";
+                resumeDevice = true;
+              };
+            };
+            */
+            root = {
+              name = "root";
+              size = "100%";
+              content = {
+                type = "lvm_pv";
+                vg = "root_vg";
+              };
             };
           };
-          /*
-             swap = {
-            size = "4G";
-            content = {
-              type = "swap";
-              resumeDevice = true;
+        };
+      };
+      raid-disk-1 = {
+        type = "disk";
+        device = "/dev/disk/by-id/ata-ST18000NM003D-3DL103_ZVTAZSKF";
+        content = {
+          type = "gpt";
+          partitions = {
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "raidpool";
+              };
             };
           };
-          */
-          root = {
-            name = "root";
-            size = "100%";
-            content = {
-              type = "lvm_pv";
-              vg = "root_vg";
+        };
+      };
+      raid-disk-2 = {
+        type = "disk";
+        device = "/dev/disk/by-id/ata-ST18000NM003D-3DL103_ZVT9SZNJ";
+        content = {
+          type = "gpt";
+          partitions = {
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "raidpool";
+              };
             };
           };
         };
@@ -67,6 +103,26 @@
                 };
               };
             };
+          };
+        };
+      };
+    };
+    zpool = {
+      raidpool = {
+        type = "zpool";
+        mode = "raidz";
+        rootFsOptions = {
+          compression = "zstd";
+          "com.sun:auto-snapshot" = "false";
+        };
+        mountpoint = null;
+        postCreateHook = "zfs snapshot raidpool@blank";
+
+        datasets = {
+          raidset = {
+            type = "zfs_fs";
+            options.mountpoint = "legacy";
+            mountpoint = "/raid";
           };
         };
       };

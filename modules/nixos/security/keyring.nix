153a4cc1b826ff897ec35e0e91bbfbee73d9c8d4
upidapi <videw@icloud.com>
2025-12-20 17:29:28 +0100

    feat) add (gnome) keyring

---
+++ modules/nixos/security/keyring.nix
@@ -0,0 +1,17 @@
+{
+  config,
+  lib,
+  mlib,
+  ...
+}: let
+  inherit (lib) mkIf;
+  inherit (mlib) mkEnableOpt;
+  cfg = config.modules.nixos.security.keyring;
+in {
+  options.modules.nixos.security.keyring = mkEnableOpt "";
+
+  config = mkIf cfg.enable {
+    # FROM: https://wiki.nixos.org/wiki/Secret_Service
+    services.gnome.gnome-keyring.enable = true;
+  };
+}

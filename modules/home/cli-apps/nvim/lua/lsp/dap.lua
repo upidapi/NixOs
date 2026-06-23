57098afd8af6065e983cd2121314719fcf68bf9e
upidapi <videw@icloud.com>
2026-06-23 23:26:49 +0200

    auto hide/show Neotree on dap-ui open/close

--- modules/home/cli-apps/nvim/lua/lsp/dap.lua
+++ modules/home/cli-apps/nvim/lua/lsp/dap.lua
@@ -24,9 +24,6 @@ require("nvim-dap-virtual-text").setup()
 local dapui = require("dapui")
 dapui.setup()
 
-dap.listeners.after.event_initialized["dapui_config"] = function()
-    dapui.open()
-end
 
 -- dap.listeners.before.event_terminated["dapui_config"] = function()
 --     dapui.close()
@@ -39,3 +36,38 @@ end
 -- NOTE: configs here https://github.com/mfussenegger/nvim-dap
 --
 -- https://igorlfs.github.io/neovim-cpp-dbg
+--
+--
+
+
+-- Close/open neotree on dap ui close/open
+local neotree_was_open = false
+
+local function is_neotree_open()
+  for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+    local buf = vim.api.nvim_win_get_buf(win)
+    if vim.bo[buf].filetype == "neo-tree" then
+      return true
+    end
+  end
+  return false
+end
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  neotree_was_open = is_neotree_open()
+  if neotree_was_open then
+    vim.cmd("Neotree close")
+  end
+  dapui.open()
+end
+
+local function restore_layout_on_close()
+  dapui.close()
+  if neotree_was_open then
+    vim.cmd("Neotree show") -- reopen without stealing cursor focus
+    neotree_was_open = false
+  end
+end
+
+dap.listeners.before.event_terminated["dapui_config"] = restore_layout_on_close
+dap.listeners.before.event_exited["dapui_config"] = restore_layout_on_close
